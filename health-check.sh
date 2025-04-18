a34d263924790ffe3b98b3a2c30ec0112be316b3
ad <hi@arnaudiaz.com>
2025-04-18 16:53:58 +0200

    health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -1,10 +1,11 @@
 #!/opt/homebrew/bin/bash
 set -euo pipefail
 
+host=google.com
 count=1
 seconds=1
 
-if ping -c $count -i $seconds google.com &> /dev/null; then
+if ping -c $count -i $seconds $host &> /dev/null; then
     exit 0
 else
     exit 1
