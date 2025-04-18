687c261cf13a2fc9deed8f1bec4eb660a1aa87ef
ad <hi@arnaudiaz.com>
2025-04-18 17:05:54 +0200

    retry

--- health-check.sh
+++ health-check.sh
@@ -7,14 +7,12 @@ seconds=1
 max_retry=5
 retry_delay_seconds=1
 
-retry=1
-while (( retry <= max_retry )); do
+for ((retry=0; retry<max_retry; retry++)); do
     echo "Retry $retry: pinging $host..."
     if ping -c $count -i $seconds $host ; then
         exit 0
     else
         sleep "$retry_delay_seconds"
-        ((retry++))
     fi
 done
 
