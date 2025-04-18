8b77852766584a80fe987561626a001039681ef1
ad <hi@arnaudiaz.com>
2025-04-18 16:13:41 +0200

    env-config with getopts

---
+++ env-config-getopts.sh
@@ -0,0 +1,43 @@
+#!/opt/homebrew/bin/bash
+set -euo pipefail
+
+curr_env="UNSET"
+
+usage() {
+    echo "usage: $0 -e (DEV, PRE, PRO)"
+    exit 1
+}
+
+while getopts ":e:" option; do
+    case $option in
+        e)
+            if [[ -z "${OPTARG:-}" ]]; then
+                echo "missing argument for -e"
+                usage
+            fi
+            case ${OPTARG} in
+                DEV) curr_env=$OPTARG;;
+                PRE) curr_env=$OPTARG;;
+                PRO) curr_env=$OPTARG;;
+                *) 
+                    echo "invalid env: ${OPTARG}"
+                    usage
+            esac
+            ;;
+        \?)
+            echo "unknown option: -$OPTARG"
+            usage
+            ;;
+        :)
+            echo "missing argument for -$OPTARG"
+            usage
+            ;;
+    esac
+done
+
+if [[ "$curr_env" = "UNSET" ]]; then
+    echo "environment not set"
+    usage
+fi
+
+echo "$curr_env"
